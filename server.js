6910042eef628c9de7440c8b774b9ecb0cdf2ec5
clstanton <cstanton77@gmail.com>
2020-06-14 12:00:27 -0500

    add post route

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const inputCheck = require('./utils/inputCheck');
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -67,6 +69,32 @@ app.delete('/api/candidate/:id', (req, res) => {
         message: 'successfully deleted',
         changes: this.changes
       });
+    });
+  });
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+        });
     });
   });
 
